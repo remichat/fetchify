6f42e0b4faa734c8bf82083e261bab64b2136e08
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-13 12:19:56 +0100

    added new method to refresh oauth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def refresh_token_from_spotify
+    return unless should_refresh?
+
+    self.access_token = new_token["access_token"]
+    self.expires_at = Time.now + 3600
+    save
+  end
+
+  private
+
+  def should_refresh?
+    expires_at < Time.now
+  end
+
+  def new_token
+    url = "https://accounts.spotify.com/api/token"
+    body = {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token
+    }
+    authorization = Base64.strict_encode64("#{ENV['SPOTIFY_CLIENT_ID']}:#{ENV['SPOTIFY_CLIENT_SECRET']}")
+    headers = { Authorization: "Basic #{authorization}" }
+    response = RestClient.post(url, body, headers)
+    JSON.parse(response.body)
+  end
 end
