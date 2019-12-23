736742d9f45caa7a12514e8dd71bb7a33a196211
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-23 15:40:38 +0100

    fake commit

--- app/services/selenium_service.rb
+++ app/services/selenium_service.rb
@@ -3,7 +3,7 @@ class SeleniumService
     binding.pry
     Selenium::WebDriver::Firefox::Service.driver_path = './config/geckodriver'
     driver = Selenium::WebDriver.for :firefox
-    driver.navigate.to "http://google.com"
+    driver.navigate.to "http://google.fr"
     tmp = driver.title
     driver.quit
     tmp
