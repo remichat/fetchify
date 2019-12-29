838938810d8f0cb827295c52691d7ddcacf5dc7f
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-29 17:55:34 +0100

    added zipper job which launches at the end of song downloads

--- app/workers/zipper_job.rb
+++ app/workers/zipper_job.rb
@@ -3,28 +3,29 @@ class ZipperJob < ApplicationJob
 
   def perform(download_id)
     download = Download.find(download_id)
-    songs = download.songs
-
-    # IL FAUT FOUTRE LES VRAIS FICHIERS DE SON ET NON PAS LES FICHEIRS EN DUR
-    # L'URL DE DL NE DURE QUE 5 MINUTES => IL FAUT LA RECUPERER AU MOMENT DE L'AFFICHAGE
-    # IL FAUT CHANGER LE NOM DE FICHIER PAR LE NOM DU SON
-    # IL FAUDRAIT EDITER LES METADATA ( A VOIR SI NOUVELLE GEM OU SI .METADATA)
-    # IL FAUT CACHER L'URL DE DOWNLOAD
-
-    folder = "./public/songs/"
-    input_filenames = ['2.mp3', '3.mp3']
 
     zipfile_name = "./public/downloads/#{download.id}.zip"
+    tmp_path = "./public/tmp_songs/#{download.id}"
+    Dir.mkdir(tmp_path)
 
     require 'zip'
     Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-      input_filenames.each do |filename|
-        zipfile.add(filename, File.join(folder, filename))
+      download.songs.each do |song|
+
+        file_path = "#{tmp_path}/#{song.file.blob.filename}"
+        File.open(file_path, 'wb') do |file|
+          file.write(song.file.download)
+        end
+
+        zipfile.add(song.file.blob.filename, file_path)
       end
     end
 
-    download.file.attach(io: File.open("./public/downloads/#{download.id}.zip"), filename: "#{download.id}.zip")
-    download.public_location = Rails.application.routes.url_helpers.rails_blob_path(download.file, only_path: true)
+    FileUtils.remove_dir(tmp_path)
+
+    download.file.attach(io: File.open(zipfile_name), filename: "#{download.id}.zip")
+    # download.public_location = Rails.application.routes.url_helpers.rails_blob_path(download.file, only_path: true)
+    download.status = Download::STATUSES[:ready]
     download.save
   end
 end
