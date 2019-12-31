6c9cbcbd647562ca85ea990d90ca09bc980c85ea
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-31 11:26:44 +0100

    changed notice to avoid errors

--- app/javascript/components/notice_popup.js
+++ app/javascript/components/notice_popup.js
@@ -1,8 +1,7 @@
 const displayNotice = (message) => {
-  const noticeElement = document.getElementById('notice-popup');
-  noticeElement.classList.remove('hidden');
-  const textElement = document.querySelector('#notice-popup > span');
-  textElement.innerText = message;
+  const notice = `<div class="alert alert-info alert-dismissible fade show m-1" role="alert">${message}<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`;
+  const body = document.querySelector('body');
+  body.insertAdjacentHTML('beforeend', notice);
 }
 
 export default displayNotice;
