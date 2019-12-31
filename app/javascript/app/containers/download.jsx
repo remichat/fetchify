3ceccc915225e1d7138b2a7153e84fb3e34bff1c
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-31 15:38:27 +0100

    added download layout

--- app/javascript/app/containers/download.jsx
+++ app/javascript/app/containers/download.jsx
@@ -4,13 +4,22 @@ import { connect } from 'react-redux';
 
 class Download extends Component {
   render() {
+    const style= {
+     backgroundImage: `url(https://mosaic.scdn.co/300/10d79086d13664d7631a1a3dda4662286ffc2a30ab67616d0000b273b94203efb252305b712e3538ab67616d0000b273c709fcd1fa691104f7d9916fab67616d0000b273f81935f794c1112bbda4de81)`
+    }
+
     return (
       <div className="download-card">
-        <div className="download-cover"></div>
-        <div className="download-info">
-
+          <div className="download-cover" style={style}>
+            <div className="cover-gradient">
+              <span className="songs-number"></span>
+              <div className="download-cta">
+                <i class="fas fa-cloud-download-alt"></i>
+              </div>
+              <span className="download-size">432 Mo</span>
+            </div>
+          </div>
         </div>
-      </div>
     );
   }
 }
