9536ab6769395a08ba9ef6c2aadbb058e558a697
Rémi Chatenay <remi.chatenay@gadz.org>
2020-06-17 15:53:44 +0200

    refacto cart.js

--- app/javascript/app/containers/cart.jsx
+++ app/javascript/app/containers/cart.jsx
@@ -1,70 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import classNames from 'classnames';
 
 import SongSmall from '../components/song_small';
 import { removeAllSongsFromCart, createDownload } from '../actions';
 import displayNotice from '../../components/notice_popup';
 
-class Cart extends Component {
-  state = {
-    collapsed: true,
-    chevronClass: "up",
-    songsListClass: "cart-collapsed"
+const  Cart = (props) => {
+  const [collapsed, setCollapsed] = React.useState(true)
+
+  const { removeAllSongsFromCart, createDownload, cartSongs } = props
+
+  const removeAllSongs = () => {
+    removeAllSongsFromCart();
   }
 
-  toggleCollapse = () => {
-    if (this.state.collapsed) {
-      this.setState({
-        collapsed: false,
-        chevronClass: "down",
-        songsListClass: ""
-      });
-
-    } else {
-      this.setState({
-        collapsed: true,
-        chevronClass: "up",
-        songsListClass: "cart-collapsed"
-      });
-    }
-  }
-
-  removeAllSongs = () => {
-    this.props.removeAllSongsFromCart();
-  }
-
-  createDownload = () => {
-    if (this.props.cartSongs.length > 0) {
-      this.props.createDownload(this.props.cartSongs);
+  const startDownload = () => {
+    if (cartSongs.length > 0) {
+      createDownload(cartSongs);
       displayNotice('Your tracks are being prepared. Check your downloads in a moment.');
     }
   }
 
-  render() {
-    return (
-      <div id="cart">
-        <div className="cart-header" onClick={this.toggleCollapse}>
-          <i className="fas fa-shopping-cart"></i>
-          <span>{this.props.cartSongs.length} TRACKS SELECTED</span>
-          <i className={`fas fa-chevron-${this.state.chevronClass}`}></i>
-        </div>
-        <div className={`songs-list-small ${this.state.songsListClass}`}>
-          <div className="songs-actions">
-            <div id="songs-select">
-              <span id="select-all" onClick={this.removeAllSongs}>Remove all</span>
-            </div>
-              <span id="download-songs" onClick={this.createDownload}><i className="fas fa-shopping-cart"></i> Prepare download</span>
-          </div>
-          <div className="separator-small">
-            <div></div>
-            <div></div>
-          </div>
-          {this.props.cartSongs.map(song => <SongSmall key={song.id} details={song} />)}
-        </div>
+  return (
+    <div id="cart">
+      <div className="cart-header" onClick={() => setCollapsed(!collapsed)}>
+        <i className="fas fa-shopping-cart"></i>
+        <span>{cartSongs.length} TRACKS SELECTED</span>
+        <i className={classNames('fas', { 'fa-chevron-up': collapsed, 'fa-chevron-down': !collapsed })}></i>
       </div>
-    );
-  }
+      <div className={classNames('songs-list-small', { 'cart-collapsed': collapsed })}>
+        <div className="songs-actions">
+          <div id="songs-select">
+            <span id="select-all" onClick={removeAllSongs}>Remove all</span>
+          </div>
+            <span id="download-songs" onClick={startDownload}>
+              <i className="fas fa-shopping-cart"/> Prepare download
+            </span>
+        </div>
+        <div className="separator-small">
+          <div></div>
+          <div></div>
+        </div>
+        {cartSongs.map(song => <SongSmall key={song.id} details={song} />)}
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
