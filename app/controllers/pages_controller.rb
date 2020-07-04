497f37c04561774a6371cb1b77da247f38e897a3
Rémi Chatenay <remi.chatenay@gadz.org>
2020-07-05 01:42:25 +0200

    adds temp bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:login]
 
   def home
+    Song.find(1000000000)
   end
 
   def settings
--- app/javascript/app/components/PanelSettings.js
+++ app/javascript/app/components/PanelSettings.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import { hasAccessToken } from '../helpers';
+import { hasAccessToken } from '../helpers'
 
 const handleNoToken = () => {
   if (hasAccessToken()) {
     return <p>You are already connected with Spotify</p>
   } else {
-    return <a className="btn btn-spotify" rel="nofollow" data-method="post" href="/auth/spotify">Connect with Spotify</a>
+    return (
+      <a className="btn btn-spotify" rel="nofollow" data-method="post" href="/auth/spotify">
+        Connect with Spotify
+      </a>
+    )
   }
 }
 
 const PanelSettings = () => {
-
   return (
     <div className="settings">
+      {window.totoProd()}
       <div className="section-settings">
-        <h3><i className="fas fa-unlock"></i> Account Management</h3>
-        <a className="btn" rel="nofollow" data-method="delete" href="/users/sign_out">Sign out</a>
+        <h3>
+          <i className="fas fa-unlock"></i> Account Management
+        </h3>
+        <a className="btn" rel="nofollow" data-method="delete" href="/users/sign_out">
+          Sign out
+        </a>
       </div>
 
       <div className="section-settings">
-        <h3><i className="fab fa-spotify"></i> Spotify connection</h3>
+        <h3>
+          <i className="fab fa-spotify"></i> Spotify connection
+        </h3>
         {handleNoToken()}
       </div>
     </div>
   )
 }
 
-
-export default PanelSettings;
+export default PanelSettings
